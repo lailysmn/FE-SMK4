92062c74aab293798a5ff14e1acf380c1d299dac
lailysmn <lailayasminfitria@gmail.com>
2023-11-09 10:39:18 +0700

    first

---
+++ 2-catatan-belanja/src/App.jsx
@@ -0,0 +1,97 @@
+const groceyItems = [
+  {
+    id: 1,
+    name: "Kopi Bubuk",
+    quantity: 2,
+    checked: true,
+  },
+  {
+    id: 2,
+    name: "Gula Pasir",
+    quantity: 5,
+    checked: false,
+  },
+  {
+    id: 3,
+    name: "Air Mineral",
+    quantity: 3,
+    checked: false,
+  },
+  {
+    id: 4,
+    name: "Air segar",
+    quantity: 3,
+    checked: false,
+  },
+];
+
+export default function App() {
+  return (
+    <div className="app">
+      <Header />
+      <Form />
+      <Grocey />
+      <footer />
+    </div>
+  );
+}
+
+function Header() {
+  return <h1>Catatan Belanjaku 📝</h1>;
+}
+
+function footer() {
+  return (
+    <footer className="stats">
+      Ada 10 barang di daftar belanjaan, 5 barang sudah dibeli (50%)
+    </footer>
+  );
+}
+
+function Form() {
+  return (
+    <form className="add-form">
+      <h3>Hari ini belanja apa kita?</h3>
+      <div>
+        <select>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+        </select>
+        <input type="text" placeholder="nama barang..." />
+      </div>
+      <button>Tambah</button>
+    </form>
+  );
+}
+
+function Grocey() {
+  return (
+    <>
+      <div className="list">
+        <ul>
+          {groceyItems.map((item) => (
+            <li key={item.id}>
+              <input type="checkbox" checked={item.checked} />
+              <span style={item.checked ? { textDecoration: "line-through" } : {}}>
+                
+                {item.quantity} {item.name}
+              </span>
+              <button>&times;</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="actions">
+        <select>
+          <option value="input">Urutkan berdasarkan urutan input</option>
+          <option value="name">Urutkan berdasarkan nama barang</option>
+          <option value="checked">Urutkan berdasarkan ceklis</option>
+        </select>
+        <button>Bersihkan Daftar</button>
+      </div>
+    </>
+  );
+}
